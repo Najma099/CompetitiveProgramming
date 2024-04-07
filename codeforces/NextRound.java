60b420ed8e808cc9a80d9507362f6e9897a4dd13
Najma099 <najmakhatun0999@gmail.com>
2024-04-07 23:11:44 +0530

    codeforces

--- codeforces/NextRound.java
+++ codeforces/NextRound.java
@@ -12,7 +12,7 @@ public class NextRound {
         int count=0;
         for(int i=0;i<n;i++)
         {
-            if(arr[i]>k)
+            if((arr[i]>=arr[k-1]) && (arr[i]>0))
             count++;
         }
         System.out.println(count);
--- codeforces/Queue.java
+++ codeforces/Queue.java
@@ -5,7 +5,17 @@ public class Queue {
         {
             Scanner sc= new Scanner(System.in);
             int n=sc.nextInt();
-            String 
+            String S=sc.next();
+            for(int i=0;i<k;i++)
+            {
+                for(int j=0;j<S.length()-1;j++)
+                {
+                    if(S.charAt[j]=="B" && S.charAt[j+1]=="G")
+                    {
+                        j+=2;
+                    }
+                }
+            }
         }
     }
 }
