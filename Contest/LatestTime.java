7aa9a3d9f3211a01f1cf57099333139c679866c2
Najma099 <najmakhatun0999@gmail.com>
2024-04-14 10:04:46 +0530

    leetcode_biweekly_contest

---
+++ Contest/LatestTime.java
@@ -0,0 +1,38 @@
+public class LatestTime {
+    public String findLatestTime(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        if(sb.charAt(0)=='?' && sb.charAt(1)=='?')
+        {
+            sb.setCharAt(0,'1');
+            sb.setCharAt(1,'1');
+        }
+        else if(sb.charAt(0)=='?')
+        {
+            if(sb.charAt(1)=='0' || sb.charAt(1)=='1')
+                sb.setCharAt(0,'1');
+            else
+                sb.setCharAt(0,'0');
+        }
+        else if(sb.charAt(1)=='?')
+        {
+            if(sb.charAt(0)=='1')
+                sb.setCharAt(1,'1');
+            else
+                sb.setCharAt(1,'9');
+        }
+        if(sb.charAt(3)=='?' && sb.charAt(4)=='?')
+        {
+            sb.setCharAt(3,'5');
+            sb.setCharAt(4,'9');
+        }
+        else if(sb.charAt(3)=='?')
+        {
+            sb.setCharAt(3,'5');
+        }
+        else if(sb.charAt(4)=='?')
+        {
+            sb.setCharAt(4,'9');
+        }
+        return sb.toString();
+    }
+}
