a59f56a7bc71dc6955e9b0e414b01e2a9856956a
Najma099 <najmakhatun0999@gmail.com>
2024-07-14 23:30:10 +0530

    upsolve

--- codeforces/Gorilla.java
+++ codeforces/Gorilla.java
@@ -10,37 +10,17 @@ public class Gorilla {
             int m = sc.nextInt();
             int k = sc.nextInt();
             
-            int[] arr = new int[n];
-            
-            // Initialize array with numbers 1 to n
-            for (int i = 0; i < n; i++) {
-                arr[i] = i + 1;
+            for(int i = n; i >= k; i--) {
+                System.out.print(i + " ");
             }
-            
-            // Adjust array elements
-            arr[0] = k;
-            arr[n - 1] = m + 1;
-            
-            // Ensure all elements are distinct
-            if (arr[0] == arr[n - 1]) {
-                // Swap arr[n-1] with the last element in the initial array
-                for (int i = 0; i < n - 1; i++) {
-                    if (arr[i] != k && arr[i] != m + 1) {
-                        int temp = arr[i];
-                        arr[i] = arr[n - 1];
-                        arr[n - 1] = temp;
-                        break;
-                    }
-                }
+            for(int i = m+1; i < k; i++) {
+                System.out.print(i + " ");
             }
-            
-            // Output the permutation
-            for (int i = 0; i < n; i++) {
-                System.out.print(arr[i] + " ");
+            for(int i = 1; i <= m ;i++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
-        
         sc.close();
     }
 }
