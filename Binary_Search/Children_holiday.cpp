18d1ba011d94d90757f7cdeb0f204abb63675a52
najma099 <najmakhatun0999@gmail.com>
2025-01-24 23:07:19 +0530

    final Childrens_Holiday

--- Binary_Search/Children_holiday.cpp
+++ Binary_Search/Children_holiday.cpp
@@ -4,31 +4,28 @@
 using namespace std;
 typedef long long ll;
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-const long long MOD = 1000000007;
 
 int m, n;
-vector<vector<int> > arr;
+vector<vector<int>> arr;
 vector<int> res;
 
 bool check(int mid) {
     vector<int> rest(n, 0);
-    int count = 0;
+    ll count = 0;
     for (int i = 0; i < n; i++) {
-        int a = 0;
-        int sum = arr[i][0] * arr[i][1] + arr[i][2];
-        int fullc = mid / sum;
-        int rem = mid % sum;
-        a = fullc * arr[i][1];
-        a += min(arr[i][1], rem / arr[i][0]);
+        ll sum = 1LL * arr[i][0] * arr[i][1] + arr[i][2];
+        ll fullc = mid / sum;
+        ll rem = mid % sum;
+        int a = fullc * arr[i][1];
+        a += min(arr[i][1], (int)(rem / arr[i][0]));
         rest[i] = a;
         count += a;
     }
     if (count >= m) {
         res = rest;
         return true;
-    } else {
-        return false;
     }
+    return false;
 }
 
 int solve() {
@@ -55,12 +52,13 @@ int solve() {
         }
     }
 
-    cout << ans << endl;
+    cout << ans << '\n';
     int tot = 0;
     for (int i = 0; i < n; i++) {
         if (tot + res[i] > m) {
             cout << m - tot << " ";
             tot = m;
+            break; 
         } else {
             cout << res[i] << " ";
             tot += res[i];
