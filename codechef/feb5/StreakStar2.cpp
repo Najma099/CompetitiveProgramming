0f4cf503e24294a9831f56cd20fb2c342c9dca64
najma099 <najmakhatun0999@gmail.com>
2025-02-14 13:25:05 +0530

    Strear_Star

---
+++ codechef/feb5/StreakStar2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+typedef long long ll;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const long long MOD = 1000000007;
+
+
+int solve() {
+    IOS
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,k;
+        cin >> n >> k;
+        vector < int> arr(n), prev(n, 1), suffix(n, 1);
+        for(int i = 0; i < n; i++) cin >> arr[i];
+
+        
+
+    }
+    return 0;
+}
+
+int main() {
+    solve();
+    return 0;
+}
